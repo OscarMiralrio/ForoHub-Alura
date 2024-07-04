14c1a464e22ca994ec783395424fcfc8425da20c
Oscar Sanchez M <skrsm_12@hotmail.com>
2024-07-04 13:43:41 -0600

    creacion addTopic en TopicController

--- src/main/java/com/alura/forohub/controller/TopicController.java
+++ src/main/java/com/alura/forohub/controller/TopicController.java
@@ -1,12 +1,27 @@
 package com.alura.forohub.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alura.forohub.domain.topics.TopicDTO;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topics")
+@Slf4j
 public class TopicController {
 
+    @PostMapping
+    public ResponseEntity addTopic(
+            @RequestHeader @Parameter(hidden = true) HttpHeaders headers,
+            @RequestBody @Valid TopicDTO topic
+            ){
+        log.info("Inicio");
 
+        log.info(("Fin"));
+        return ResponseEntity.ok().build();
+    }
 
 }
---
+++ src/main/java/com/alura/forohub/domain/topics/TopicDTO.java
@@ -0,0 +1,8 @@
+package com.alura.forohub.domain.topics;
+
+public record TopicDTO(
+        String message,
+        String title,
+        String course
+) {
+}
