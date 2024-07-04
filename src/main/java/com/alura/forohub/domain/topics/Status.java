436eee1b81a7aa7fdab9520f8e39ae86dc58c970
Oscar Sanchez M <skrsm_12@hotmail.com>
2024-07-04 12:58:43 -0600

    Creacion de la entidad Topic

---
+++ src/main/java/com/alura/forohub/domain/topics/Status.java
@@ -0,0 +1,20 @@
+package com.alura.forohub.domain.topics;
+
+public enum Status {
+
+    NUEVO,
+    ABIERTO,
+    EN_PROCESO,
+    PENDIENTE,
+    RESUELTO,
+    VERIFICADO,
+    CERRADO,
+    REABIERTO,
+    EN_ESPERA,
+    RECHAZADO,
+    DUPLICADO,
+    ARCHIVADO
+
+
+
+}
---
+++ src/main/java/com/alura/forohub/domain/topics/TopicRepository.java
@@ -0,0 +1,6 @@
+package com.alura.forohub.domain.topics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+}
