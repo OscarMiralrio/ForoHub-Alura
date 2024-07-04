36f343e37cee2b3a4dab6de1096432d114d85337
Oscar Sanchez M <skrsm_12@hotmail.com>
2024-07-04 11:35:06 -0600

    Modificacion generarToken().withIssuer

--- src/main/java/com/alura/forohub/infra/security/TokenService.java
+++ src/main/java/com/alura/forohub/infra/security/TokenService.java
@@ -23,7 +23,7 @@ public class TokenService {
         try {
             Algorithm algorithm = Algorithm.HMAC256(apiSecret);
             return JWT.create()
-                    .withIssuer("voll med")
+                    .withIssuer("Foro Hub")
                     .withSubject(user.getUsername())
                     .withClaim("id", user.getId())
                     .withExpiresAt(generarFechaExpiracion())
