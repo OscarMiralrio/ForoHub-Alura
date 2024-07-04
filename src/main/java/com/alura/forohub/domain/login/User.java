468c9f74ce1d97a14539663fe1bf013569aeb157
Oscar Sanchez M <skrsm_12@hotmail.com>
2024-07-03 22:14:09 -0600

    creacion de tabla / entity user

---
+++ src/main/java/com/alura/forohub/domain/login/User.java
@@ -0,0 +1,21 @@
+package com.alura.forohub.domain.login;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+}
